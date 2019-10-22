b452f161251acb37050c481d69b77fa83aa5cb55
Danilo Barros Cardoso <danilobcardoso@yahoo.com.br>
2019-10-22 11:28:53 -0300

    Removendo chaves

--- params.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = '958706110:AAGdhc_eRu7zyVouZ7DKtOqGNwi8E78-IJs'
-BOT_ID = '1'
-RESTART_TOMCAT_COMMAND = 'service tomcat9 restart'
-SERVICE_URL='http://buenosaires.infobus.br.engineering/app/rest/transit/vehicle-position'
-VALID_PERIOD=60
-IS_FEED=True
