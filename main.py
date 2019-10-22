b00435088bd2b9e100143e782537bafe6df596c8
danilocardoso <danilo.cardoso@dcc.ufmg.br>
2019-10-22 10:55:55 -0300

    Melhorias

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import tomcat_bot
+import threading
+import time
+import schedule
+
+
+# def thread_function(bot):
+    # time.sleep(30)
+    # bot.send_message(chat_id=-322840871, text="Deu certo caralho!")
+
+
+#  print("Thread %s: finishing", param)
 
 
 if __name__ == '__main__':
-    bot = tomcat_bot.TomcatBot()
-    bot.start()
-
-
+    tomcat_bot = tomcat_bot.TomcatBot()
+    tomcat_bot.start()
+    schedule.every().minute.do(tomcat_bot.check_service)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
--- tomcat_bot.py
+++ tomcat_bot.py
@@ -6,10 +6,19 @@ import os
 import shutil
 import logging
 import params
+import requests
+import datetime
+import feed_validators as feed
 
 logging.basicConfig(level=logging.INFO)
 
 class TomcatBot:
+
+    STATUS_MESSAGE = 'status'
+    CHECK_MEMORY_MESSAGE = 'Verificar memória'
+    CHECK_DISK_SPACE_MESSAGE = 'Verificar espaço em disco'
+    RESTART_TOMCAT_MESSAGE = 'Reiniciar tomcat'
+    CHECK_LAST_LOG_ENTRIES_MESSAGE = ' Verificar últimas mensagens no log'
 
     def __init__(self):
         logging.info("Iniciando conexão com o BOT")
@@ -18,13 +27,16 @@ class TomcatBot:
         echo_handler = MessageHandler(Filters.all, self.process_message)
         self.dispatcher.add_handler(echo_handler)
 
+    def bot(self):
+        return self.dispatcher.bot
+
     def start(self):
         logging.info("Iniciando monitoramento das mensagens")
         self.updater.start_polling()
 
     def send_options_massage(self, update, context):
-        custom_keyboard = [['Reiniciar Tomcat', 'Ultimas linhas do log'],
-                           ['Verificar espaço disco', 'Verificar memoria']]
+        custom_keyboard = [[self.RESTART_TOMCAT_MESSAGE, self.CHECK_LAST_LOG_ENTRIES_MESSAGE],
+                           [self.CHECK_DISK_SPACE_MESSAGE, self.CHECK_MEMORY_MESSAGE]]
 
         reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
         context.bot.send_message(chat_id=update.effective_chat.id,
@@ -72,37 +84,80 @@ class TomcatBot:
 
     def check_status(self, update, context):
         reply_markup = telegram.ReplyKeyboardRemove()
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Eu estou vivo!",
-                                 reply_markup=reply_markup)
+        URL = params.SERVICE_URL
+        r = requests.get(url=URL)
+        data = r.text
+        if params.IS_FEED:
+            report = feed.quick_report(data)
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text=report,
+                                     reply_markup=reply_markup)
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Eu estou vivo!",
+                                     reply_markup=reply_markup)
 
-    def process_message(self, update, context):
+    def is_reply_message(self, update, context):
+        if update.message.reply_to_message is not None:
+            if update.message.reply_to_message.from_user.username == context.bot.username:
+                return True
+        return False
+
+    def process_reply_message(self, update, context):
         text_message = update.message.text
-        logging.debug("Mensagem recebida: " + text_message)
-        if update.message.bot.name in text_message:
-            self.send_options_massage(update, context)
-
-        if 'Reiniciar Tomcat' in text_message:
+        if self.RESTART_TOMCAT_MESSAGE in text_message:
             logging.info("Comando REINICAR TOMCAT")
             self.restart_tomcat(update, context)
 
-        if 'Ultimas linhas do log' in text_message:
+        if self.CHECK_LAST_LOG_ENTRIES_MESSAGE in text_message:
             logging.info("Comando ULTIMAS LINHAS DO LOG")
             self.last_log_lines(update, context)
 
-        if 'Verificar espaço disco' in text_message:
+        if self.CHECK_DISK_SPACE_MESSAGE in text_message:
             logging.info("Comando Espaço em Disco")
             self.check_disk_space(update, context)
 
-        if 'Verificar memória' in text_message:
+        if self.CHECK_MEMORY_MESSAGE in text_message:
             logging.info("Comando verificar memória")
             self.check_memory(update, context)
 
-        if 'Status' in text_message:
+    def is_directed_message(self, update, context):
+        if update.message.bot.name in update.message.text:
+            return True
+        return False
+
+    def process_broadcast_message(self, update, context):
+        if self.STATUS_MESSAGE in update.message.text.lower():
             logging.info("Comando verificar status")
             self.check_status(update, context)
 
+    def process_message(self, update, context):
+        logging.debug("Mensagem recebida: " + update.message.text)
+
+        if self.is_reply_message(update, context):
+            self.process_reply_message(update, context)
+        elif self.is_directed_message(update, context):
+            self.send_options_massage(update, context)
+        else:
+            self.process_broadcast_message(update, context)
 
 
+    def check_service(self):
+        URL = params.SERVICE_URL
+        r = requests.get(url=URL)
+        data = r.text
+        if self.service_hanged(data):
+            self.dispatcher.bot.send_message(chat_id=-322840871, text="Atenção... serviço congelado!")
+        if self.service_empty(data):
+            self.dispatcher.bot.send_message(chat_id=-322840871, text="Atenção... serviço vazio!")
 
+    def service_hanged(self, data):
+        if params.IS_FEED:
+            return feed.service_hanged(data)
+        return False
+
+    def service_empty(self, data):
+        if params.IS_FEED:
+            return feed.service_empty(data)
+        return False
 
